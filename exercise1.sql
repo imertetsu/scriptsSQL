2d540967f2b6ca395a6ba3abf22396ae409f7026
imertetsu <imertpro@gmail.com>
2023-12-14 20:58:46 -0400

    exercise 3 done

--- exercise1.sql
+++ exercise1.sql
@@ -1,4 +1,4 @@
-#Execercise 1:
+#Exercise 1:
 --Table: Customer
 
 -- +-------------+---------+
@@ -21,20 +21,6 @@ SELECT c.name
 FROM Customer c
 WHERE c.referee_id IS NULL OR c.referee_id != 2;
 
-#Execercise 2:
--- Table: Products
 
--- +-------------+---------+
--- | Column Name | Type    |
--- +-------------+---------+
--- | product_id  | int     |
--- | low_fats    | enum    |
--- | recyclable  | enum    |
--- +-------------+---------+
--- product_id is the primary key (column with unique values) for this table.
--- low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
--- recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
 
-SELECT product_id 
-FROM Products
-WHERE low_fats = 'Y' AND recyclable = 'Y';
+
