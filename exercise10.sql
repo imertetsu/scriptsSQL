8c26d9792c21c0705c45f5998bf1dba50a5cbee6
imertetsu <imertpro@gmail.com>
2024-01-10 19:52:27 -0400

    exercise 10 done

--- exercise10.sql
+++ exercise10.sql
@@ -27,3 +27,16 @@
 -- Return the result table in any order.
 
 -- The result format is in the following example.
+SELECT
+    machine_id,
+    ROUND(
+        AVG(
+            CASE WHEN activity_type = 'end' THEN timestamp
+            END) -
+        AVG(
+            CASE WHEN activity_type = 'start' THEN timestamp
+            END),
+        3
+    ) AS processing_time
+FROM Activity
+GROUP BY machine_id;
